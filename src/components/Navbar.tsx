a36ac6de309e7e62063a31814e87cecf9a39b200
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-01 18:38:09 +0530

    update search-bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,10 @@ const Navbar = () => {
         <div className="container hidden lg:block">
             <div className="flex justify-between items-center p-8">
                 <h1 className="text-4xl font-medium">LOGO</h1>
+                <div className="relative w-full max-w-[500px]">
+                  <input className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full " 
+                  type="text" placeholder="Search Product..." />
+                </div>
             </div>
         </div>
     </div>
