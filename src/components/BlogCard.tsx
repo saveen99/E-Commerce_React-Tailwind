b6168f7a47942fd6b8945197e6bac1eccf1aad92
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-08 11:08:43 +0530

    Update BlogCard.tsx

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -7,7 +7,14 @@ interface propsType {
 
 const BlogCard: React.FC<propsType> = ({ img, title, date, comment }) => {
   return (
-    <div>BlogCard</div>
+    <div className="space-y-4">
+      <img className="rounded-lg hover:scale-105 transition-transform" src={img} alt="post" />
+      <div className="text-accent font-medium">
+        <span>{date} / </span>
+        <span>{comment} Comments</span>
+      </div>
+      <h3 className="font-bold text-xl">{title}</h3>
+    </div>
   )
 }
 
