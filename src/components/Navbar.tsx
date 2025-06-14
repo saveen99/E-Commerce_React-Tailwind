bcb68c0e74e53616fb30e082f952623d345ee713
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-14 17:11:44 +0530

    hotfix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,9 @@ const Navbar = ({setShowCart}: any) => {
     <div className='sticky top-0 bg-white z-10'>
         <div className="container hidden lg:block">
             <div className="flex justify-between items-center p-8">
-                <h1 className="text-4xl font-medium">Logo</h1>
+                <h1 className="text-4xl font-medium">
+                  <img className="w-full h-[50px]" src="/logo.png" alt="logo" />
+                </h1>
                 <div className="relative w-full max-w-[500px]">
                   <input className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full " 
                   type="text" placeholder="Search Product..." />
