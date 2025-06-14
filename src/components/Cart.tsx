2ce5187d9cfd1be1b163388828001974f0c8e3a6
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-14 16:17:10 +0530

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,13 +3,42 @@ import CartProduct from "./CartProduct";
 import { useCartContext } from "../context/cartContext";
 
 const Cart = ({ setShowCart }: any) => {
+  const { product } = useCartContext();
+
   return (
     <div className="bg-[#0000007d] w-full h-screen fixed left-0 top-0 z-20"
     onClick={() => setShowCart(false)}>
-        <div></div>
+        <div className="max-w-[400px] w-full h-full bg-white absolute right-0 top-0 p-6"
+        onClick={(e) => e.stopPropagation()}>
+          <RxCross1
+            className="absolute right-0 top-0 m-6 text-[24px] cursor-pointer"
+            onClick={() => setShowCart(false)}
+          />
+          <h3 className="pt-6 text-lg font-medium text-gray-600 uppercase">
+            Your Cart
+          </h3>
+
+          <div className="mt-6">
+            {product?.map((el: any) => (
+              <CartProduct
+                key={el.name}
+                img={el.img}
+                name={el.name}
+                price={el.price}
+              />
+            ))}
+          </div>
+
+          <button className="bg-accent text-white text-center w-full rounded-3xl py-2 hover:bg-accent-dark mb-4 mt-4">
+            View Cart
+          </button>
+          <button className="bg-accent text-white text-center w-full rounded-3xl py-2 hover:bg-accent-dark">
+            CheckOut
+          </button>
+        </div>
     </div>
 
   )
 }
 
-export default Cart
+export default Cart;
