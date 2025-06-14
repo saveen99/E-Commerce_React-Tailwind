cb1e411bcfd48692822b6a2f9e262e30204323c7
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-14 16:25:41 +0530

    Update CartProduct.tsx

--- src/components/CartProduct.tsx
+++ src/components/CartProduct.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { RxCross1 } from "react-icons/rx"
 
-const CartProduct = () => {
+interface propsType {
+    img: string;
+    name: string;
+    price: string;
+}
+
+const CartProduct: React.FC<propsType> = ({ img, name, price }) => {
   return (
-    <div>CartProduct</div>
+    <div className="flex justify-between items-center">
+        <div className="flex items-center gap-4">
+            <img className="h-[100px]" src={img} alt={name} />
+            <div>
+                <h3 className="font-medium">{name}</h3>
+                <p className="text-gray-600">1 x {price}</p>
+            </div> 
+        </div>
+
+        <RxCross1 />
+    </div>
   )
 }
 
-export default CartProduct
+export default CartProduct;
