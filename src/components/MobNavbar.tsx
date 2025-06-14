ce719d3767dfe0d03c02f0dd6872626c9d534eb6
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-14 16:36:22 +0530

    Update MobNavbar.tsx

--- src/components/MobNavbar.tsx
+++ src/components/MobNavbar.tsx
@@ -3,7 +3,7 @@ import { IoIosMenu } from "react-icons/io";
 import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
 import CartCountBadge from "./CartCountBadge";
 
-const MobNavbar = () => {
+const MobNavbar = ({setShowCart}: any) => {
   return (
     <div className="sticky top-0 bg-white z-10">
         <div className="container p-8 lg:hidden">
@@ -15,7 +15,7 @@ const MobNavbar = () => {
                 <h1 className="text-4xl">Logo</h1>
                 <div className="flex gap-4 text-[30px]">
                     <AiOutlineUser />
-                    <div className="relative cursor-pointer">
+                    <div className="relative cursor-pointer" onClick={() => setShowCart(true)}>
                         <AiOutlineShoppingCart />
                         <CartCountBadge size="w-[20px] h-[20px]" />
                     </div>
