f5bf858a1a5a81eb3df2696aeb08bd4546ec49af
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-08-23 21:55:55 +0530

    optimize image loading

--- src/components/FeatureSectionBreakfast.tsx
+++ src/components/FeatureSectionBreakfast.tsx
@@ -1,10 +1,14 @@
 import ProductCard from "./ProductCard";
+import product1 from "../assets/images/product__5.jpg";
+import product2 from "../assets/images/product__2.jpg";
+import product3 from "../assets/images/product__3.jpg";
+import product4 from "../assets/images/product__4.jpg";
 
 const data = [
-    { id: 0, img: "../assets/images/product__5.jpg", name: "Fresh Tomato", price: "$400" },
-    { id: 1, img: "../assets/images/product__2.jpg", name: "Crunchy Crisps", price: "$300" },
-    { id: 2, img: "../assets/images/product__3.jpg", name: "Jewel Cranberries", price: "$200" },
-    { id: 3, img: "../assets/images/product__4.jpg", name: "Almond Organic", price: "$100" },
+    { id: 0, img: product1, name: "Fresh Tomato", price: "$400" },
+    { id: 1, img: product2, name: "Crunchy Crisps", price: "$300" },
+    { id: 2, img: product3, name: "Jewel Cranberries", price: "$200" },
+    { id: 3, img: product4, name: "Almond Organic", price: "$100" },
 ];
 
 const FeatureSectionBreakfast = () => {
