63df35978419e004641142e19e2f3de864126379
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-08-31 11:15:19 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <div className="border-t border-gray-300 mt-16">
-        <div className="container text-center text-gray-500 py-4">
-            &copy; Copyrighht @Saveen Maduranga 2015
+        <div className="container text-sm text-center text-gray-500 py-4">
+            &copy; Copyrighht @Saveen Maduranga 2025
          </div>
     </div>
   )
