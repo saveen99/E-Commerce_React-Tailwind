bf1cd9835927ceef61a91a163799e9a32bec1b1c
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-08-31 10:22:17 +0530

    Update MobNavbar.tsx

--- src/components/MobNavbar.tsx
+++ src/components/MobNavbar.tsx
@@ -1,6 +1,6 @@
 import { IoSearchSharp } from "react-icons/io5";
 import { IoIosMenu } from "react-icons/io";
-import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import CartCountBadge from "./CartCountBadge";
 
 const MobNavbar = ({setShowCart}: any) => {
@@ -8,20 +8,26 @@ const MobNavbar = ({setShowCart}: any) => {
     <div className="sticky top-0 bg-white z-10">
         <div className="container p-8 lg:hidden">
             <div className="flex justify-between items-center">
+                <h1 className="text-4xl">
+                    <img className="w-full h-[35px]" src="/logo1.png" alt="logo" />
+                </h1>
+
                 <div className="flex items-center gap-6">
-                    <IoIosMenu size={30} />
                     <IoSearchSharp size={24}/>
                 </div>
-                <h1 className="text-4xl">
-                    <img className="w-full h-[50px]" src="/logo.png" alt="logo" />
-                </h1>
+
                 <div className="flex gap-4 text-[30px]">
-                    <AiOutlineUser />
                     <div className="relative cursor-pointer" onClick={() => setShowCart(true)}>
                         <AiOutlineShoppingCart />
                         <CartCountBadge size="w-[20px] h-[20px]" />
                     </div>
                 </div>
+
+                <div className="flex justify-end items-center gap-6">
+                    <IoIosMenu size={30} />
+                </div>
+                
+                
             </div>
         </div>
     </div>
